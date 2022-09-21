4d47612b5acb875c840042efd30538062ba375e1
wdlts <102986848+wdlts@users.noreply.github.com>
2022-09-21 22:54:43 +0300

    Update NewFile.py

--- NewFile.py
+++ NewFile.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-x = 2
-y = 5
-
-temp = str(lambda x, y: x*y)
-
-print(temp)
-=======
 print('Hello World!')
 print('Hello World!')
 print('Hello World!')
 print('Hello World!')
->>>>>>> developer1
